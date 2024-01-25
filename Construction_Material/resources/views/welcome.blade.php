a3591b72b8d2f5e19ab54ed126fe46ce405c3eb1
VIRAKNON <VIRAKNON.sdeungviraknon@gmail.com>
2024-01-25 23:58:58 +0700

    text in file welcome and create test register

--- Construction_Material/resources/views/welcome.blade.php
+++ Construction_Material/resources/views/welcome.blade.php
@@ -1,10 +1,44 @@
 <?php
 include '../resources/views/root/header.blade.php';
 ?>
-<link rel="stylesheet" href="{{asset('css/test.css')}}">
+<link rel="stylesheet" href="{{ asset('css/test.css') }}">
 </head>
+
 <body>
     <span class="h3">Construction Materials testing Page</span>
-</body>
-</html>
+    {{-- <table>
+        <thead>
+            <tr>
+                <th>asdfa</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
 
+    <script>
+        fetch('http://localhost/API/brand/get_brand.php')
+            .then(result => result.json())
+            .then(data => {
+                let row = '';
+                let No = 1;
+                for (let i in data) {
+                    row += `
+                <tr>
+                    <td>${No++}</td>
+                    <td>${data[i]['id']}</td>
+                    <td>${data[i]['b_name']}</td>
+                    <td>${data[i]['b_namekh']}</td>
+                    <td>${data[i]['category']}</td>
+                    <td>${data[i]['categorykh']}</td>
+                    <td>${data[i]['des']}</td>
+                    <td>${data[i]['st']}</td>
+                </tr>
+                `;
+                }
+                document.querySelector('tbody').innerHTML = row;
+            })
+    </script> --}}
+</body>
+
+</html>
--- Construction_Material/routes/web.php
+++ Construction_Material/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test/register', function () {
+    return view('test_register');
+});
